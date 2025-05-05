c9203d1c439f16248ae181ce93ab2ca6e8eff544
Grasten <aranar0faroso@gmail.com>
2025-05-05 20:01:19 +0300

    + design for the domain counter, new fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-      <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fleur+De+Leah&display=swap"
-            rel="stylesheet"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Martian+Mono:wght@600&family=Roboto+Mono&display=swap"
+        rel="stylesheet"/>
+
       <div className="parser">
         <div className="parser__topbar">
           <h1 className="parser__topbar__title font-bebas">Glorious domain parser</h1>
@@ -17,16 +19,18 @@ function App() {
             <textarea className="parser__topbar__inputs__input parser-fields" id="parserInput"></textarea>
           </div>
           <div className="parser__topbar__buttons">
-              <button className="parser__topbar__buttons__button" onClick={cleanDomains}>Get domains</button>
-            <button className="parser__topbar__buttons__button" onClick={openDomains}>Open domains</button>
+            <button className="parser__topbar__buttons__button font-martian" onClick={cleanDomains}>Get domains
+            </button>
+            <button className="parser__topbar__buttons__button" onClick={openDomains}>Open domains
+            </button>
             <button className="parser__topbar__buttons__button" onClick={cleanDomains}>Clean</button>
             <button className="parser__topbar__buttons__button" onClick={openDomains}>Open</button>
           </div>
         </div>
         <label>
           <textarea className="parser__output parser-fields" id="parserOutput"></textarea>
-          <p className="parser__output__counter" id="parserOutputCounter"></p>
         </label>
+        <p className="parser__output__counter" id="parserOutputCounter">Number of domains:</p>
       </div>
     </>
   )
