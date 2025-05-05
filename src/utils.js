b20332e27c2207401a533b04ef463e2f0644eba5
Grasten <aranar0faroso@gmail.com>
2025-05-06 00:22:32 +0300

    Pretty sure I cleaned up all the commented mess and unused libs
    First alpha test ready i guess

--- src/utils.js
+++ src/utils.js
@@ -1,73 +1,9 @@
-//import LinkifyIt from 'linkify-it';
-//import tlds from 'tlds';
 import { parse } from 'tldts';
 
-//const linkify = new LinkifyIt();
-//set({fuzzyEmail: false})
-//linkify.tlds(tlds)
-
-//let rawDomains, filteredDomainsString;
 let filteredDomainsArray = [];
-//const regBracketDot = /(\s?\W\s?\.\s?\W\s?)?(\s?\W\s?)\.(\s?\W\s?)?/ig;
 const regBracketDotText = /(\s?\W\s?)dot(\s?\W\s?)/ig;
 const regBracketDot = /(\s?\W\s?)\.(\s?\W\s?)/ig;
 const regDomains = /(?<=^|[^a-z0-9])((?:[a-z0-9-]+\.)+[a-z]{2,})(?=[^a-z0-9]|$)/gi;
-
-/*const testCases = [
-  "Check this out: /google.com is cool.",
-  "Visit us at www.example.org for more info.",
-  "Here’s a weird one: ssomfeafqhtew$something.co.uk!",
-  "What about inline?Trythisdomain.co.",
-  "Go to abc.def.net and tell me what you see.",
-  "Contact: email@example.com (should NOT match domain)",
-  "Slash style: /sub.domain.info is often ignored.",
-  "Trailing: This ends with domain.net!",
-  "Leading: $$$money-site.biz is where it's at.",
-  "In brackets [test-domain.co.uk] you go.",
-  "URL style: https://blog.company.dev/posts",
-  "Punycode style: xn--fsq.xn--0zwm56d (IDN domain)",
-  "Subdomains? Try mail.portal.example.com now.",
-  "MidwordlikeStringdomain.usisnotarealdomain",
-  "Dot at the end: use something.ai.",
-  "Wrong TLD: example.toolongtld (shouldn't match)",
-  "Short TLD: check io.io or me.me",
-  "Domain with dashes: go-to-this-site.com immediately.",
-  "Fake URL: ftp://not.a.url.com/test.txt",
-  "Sentence.with.dots.but.not.a.domain probably shouldn't match",
-];*/
-
-
-/*function removeBrackets(el){
-  el = el.replace(regBracketDot, ".");
-  el = el.replace(regBracketDotText, ".");
-  return el;
-}*/
-/*function uniq(a) {
-  const seen = {};
-  return a.filter(function(item) {
-    return Object.prototype.hasOwnProperty.call(seen, item) ? false : (seen[item] = true);
-  });
-}*/
-/*function getDomainsArrayFromText(domains) {
-  let domainsArray = [];
-  domains.matchAll(regDomains).forEach((domain) => {
-    console.log("found domain: ", domain[0]);
-    domainsArray.push(domain[0]);
-  });
-  domainsArray = uniq(domainsArray)
-  return domainsArray;
-}*/
-/*function getDomainsArrayFromText(domains) {
-  let matches = domains.match(regDomains) || [];
-  return [...new Set(matches)];
-}*/
-/*function getDomainsArray(domainString){
-  linkify.match(domainString).forEach((domain) => {
-    filteredDomainsArray.push(domain.text);
-  });
-  filteredDomainsArray = uniq(filteredDomainsArray)
-  return filteredDomainsArray;
-}*/
 
 // Converts an array of domains to a text list
 function createListFromArray(domainArray){
@@ -130,6 +66,7 @@ export function parseDomains(param){
   document.getElementById("parserOutputCounter").innerText = `Number of links: ${filteredDomainsArray.length}`;
 }
 
+// Opens parsed domains, can parse them first if needed
 export function openParsedDomains(){
   console.log(document.getElementById("parserInput").value);
   // Check if there are parsed domains
@@ -147,37 +84,3 @@ export function openParsedDomains(){
     })
   }
 }
-
-/*export function cleanDomains(){
-  rawDomains = document.getElementById("parserInput").value;
-  filteredDomainsArray = [];
-  if (rawDomains !== null) {
-    filteredDomainsString = createListFromArray(getDomainsArray(removeBrackets(rawDomains)));
-    document.getElementById("parserOutput").value = filteredDomainsString;
-    document.getElementById("parserOutputCounter").innerText = `Number of domains: ${filteredDomainsArray.length}`;
-  }
-}*/
-/*export function testExtraction(){
-  getUnfilteredDomains();
-
-  rawDomains = document.getElementById("parserInput").value;
-  filteredDomainsArray = [];
-  if (rawDomains !== null) {
-    let worklist = removeBrackets(rawDomains);
-    worklist = getDomainsArrayFromText(worklist);
-    filteredDomainsArray = worklist;
-    // Created array of domains
-    worklist = createListFromArray(worklist);
-
-    document.getElementById("parserOutput").value = worklist;
-    document.getElementById("parserOutputCounter").innerText = `Number of domains: ${filteredDomainsArray.length}`;
-  }
-}*/
-/*export function openDomains() {
-  cleanDomains();
-  if (filteredDomainsString !== null) {
-    filteredDomainsArray.forEach((el) => {
-      window.open(`https://${el}`);
-    })
-  }
-}*/
