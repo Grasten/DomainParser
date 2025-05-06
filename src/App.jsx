f49af5bc901da356b6d3147087e65924318d52b1
Grasten <aranar0faroso@gmail.com>
2025-05-06 14:51:55 +0300

    Updated design to accommodate the new functionality better

--- src/App.jsx
+++ src/App.jsx
@@ -20,20 +20,20 @@ function App() {
           <div className="parser__topbar__inputs">
             <label htmlFor="parserInput" className="parser__topbar__inputs__label"></label>
             <textarea className="parser__topbar__inputs__input parser-fields" id="parserInput"
-                      defaultValue="hXXps://domain [.] com/
-Domain TLD Source Category Suspension Date quaes[dot]store store Google Safe Browsing Social
-Engineering 2025-04-06 12:57:48 soulvibe[dot]store store Netcraft Scam 2025-04-06 14:00:42
-https://bradford.sttzik.icu/Xf?xf9=26-90
-                      ">
-
-            </textarea>
+                      defaultValue="hXXps://domain [.] com/"></textarea>
           </div>
           <div className="parser__topbar__buttons">
-            <button className="parser__topbar__buttons__button" onClick={()=> parseDomains("domain")}>Parse domains</button>
-            <button className="parser__topbar__buttons__button" onClick={openParsedDomains}>Open parsed links</button>
-            <button className="parser__topbar__buttons__button" onClick={()=> parseDomains("hostname")}>Parse hostnames</button>
-            <button className="parser__topbar__buttons__button" onClick={()=> parseDomains("url")}>Parse URLs</button>
-            <button className="parser__topbar__buttons__button" onClick={()=> console.clear()}>clear</button>
+            <button className="parser__topbar__buttons__button parser__topbar__buttons__button--domains" id="parseDomains"
+                    onClick={()=> parseDomains("domain")}>Parse domains</button>
+
+            <button className="parser__topbar__buttons__button parser__topbar__buttons__button--hostnames" id="parseHostnames"
+                    onClick={()=> parseDomains("hostname")}>Parse hostnames</button>
+
+            <button className="parser__topbar__buttons__button parser__topbar__buttons__button--URLs" id="parseURLs"
+                    onClick={()=> parseDomains("url")}>Parse URLs</button>
+
+            <button className="parser__topbar__buttons__button parser__topbar__buttons__button--open" id="parseOpen"
+                    onClick={() => openParsedDomains()}>Open parsed links</button>
           </div>
         </div>
         <label>
